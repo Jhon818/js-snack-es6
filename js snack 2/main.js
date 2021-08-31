05a6ccc39ebaf47f2fe40ef221ec7af0c5026f6d
giov <pintogiovanni818@gmail.com>
2021-08-31 17:59:14 +0200

    completo

--- js snack 2/main.js
+++ js snack 2/main.js
@@ -1,5 +1,5 @@
 // Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
-serieA = [
+let serieA = [
     {name: "Inter", puntiFatti:0, falliSubiti:0},
     {name: "Juve", puntiFatti:0, falliSubiti:0},
     {name: "Milan", puntiFatti:0, falliSubiti:0}
@@ -10,10 +10,26 @@ serieA = [
 for (const key in serieA) {
     serieA[key].puntiFatti = Math.random()*6;
     serieA[key].falliSubiti = Math.random()*6;
+    var falliSubiti = serieA[key].falliSubiti;
+    var name = serieA[key].name;
 }
-console.log(serieA)
 
 
+console.log(serieA);
 // Punti fatti e falli subiti.
 
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+// metodo col ciclo 
+// const punteggi = [];
+// for(i = 0; i < serieA.length; i++){
+// const {name,falliSubiti} = serieA[i];
+// punteggi.push({name, falliSubiti});
+// }
+// console.log(punteggi)
+
+const punteggi = [];
+ punteggi.push(falliSubiti,name);
+console.log(punteggi);
+
+
