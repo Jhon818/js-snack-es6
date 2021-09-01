d662f2431edce90d1d987cf0f14cf9d9afd746f9
giov <pintogiovanni818@gmail.com>
2021-09-01 17:56:37 +0200

    correzione

--- js snack 3/main.js
+++ js snack 3/main.js
@@ -1,13 +1,30 @@
 // Snack 3
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
-const myFunction = () => {
-     array = [1,2,3,4,5]; 
-    a = 1;
-    b = 3;
-}
-  myFunction();
+// const myFunction = () => {
+//     array = [1,2,3,4,5]; 
+//     a = 1;
+//     b = 3;
+// }
+//   myFunction();
 
-    const newArray = array.filter((element) => {
+//     const newArray = array.filter((element) => {
+     
+//         if (element > a && element < b){
+//             return true
+//         }
+//         else {
+//             return false
+//         }  
+//     });
+//     console.log(newArray);
+
+// versione corretta
+let array = [1,2,3,4,5]; 
+let a = 1;
+let b = 3;
+
+const myFunction = (array, a, b) => {
+   const newArray = array.filter((element) => {
      
         if (element > a && element < b){
             return true
@@ -16,7 +33,12 @@ const myFunction = () => {
             return false
         }  
     });
-    console.log(newArray);
+    return newArray;
+}
+
+myFunction(array, a, b);
+// versione corretta
+    
 
 
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
